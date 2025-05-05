69a652eec30aed696eec65c6ec2e0ccb43fc1cbf
Ishant Arora <ishantarora311@gmail.com>
2025-05-05 20:36:56 +0530

    Error corrected

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,7 @@
 const express = require("express");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const authRoutes = require("./routes/authRoutes.js");
 const userRoutes = require("./routes/userRoutes.js");
 const postRoutes = require("./routes/postRoutes.js");
