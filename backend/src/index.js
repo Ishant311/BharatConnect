e2881f574511d913cbf6c80b0076502a1196ae95
Ishant Arora <ishantarora311@gmail.com>
2025-05-05 20:33:10 +0530

    Error corrected

--- backend/src/index.js
+++ backend/src/index.js
@@ -23,6 +23,11 @@ app.use(cookieParser());
 app.use("/api/auth",authRoutes);
 app.use("/api/user",userRoutes);
 app.use("/api/post",postRoutes);
+app.use(express.static(path.join(__dirname, 'Frontend/build')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'Frontend/build', 'index.html'));
+});
 
 
 
